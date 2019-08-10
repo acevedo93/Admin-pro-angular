c489db336658c3aceba9d2f1829642af94a78431
david <dacevedojaramillo@gmail.com>
2019-08-10 13:59:51 -0500

    agregando y modificando moduloes

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafics1Component } from './grafics1/grafics1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { NoPageFoundComponent } from '../no-page-found/no-page-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'grafics', component: Grafics1Component},
+      {path: 'progress', component: ProgressComponent},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+      {path: '**', component: NoPageFoundComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
